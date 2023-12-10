eafe757eddb444bd03d1d8e6bea728db318fe07a
Andrey Zlotnikov <andrey.zlotnikov@testpro.io>
2023-12-10 12:55:33 -0600

    Update Homework20.java

--- src/test/java/Homework20.java
+++ src/test/java/Homework20.java
@@ -23,9 +23,7 @@ public class Homework20 extends BaseTest {
     @Test
     public void deletePlaylist () throws InterruptedException {
         logIn("demo@class.com", "te$t$tudent");
-        //Thread.sleep(3000);
         createPlaylist("Experimental");
-        //Thread.sleep(3000);
         deleteSelectedPlaylist("Experimental");
 
     }
