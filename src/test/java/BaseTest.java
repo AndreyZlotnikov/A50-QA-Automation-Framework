98a0d590c8df12c10550ea2b17df33990b9e7cba
Andrey Zlotnikov <andrey.zlotnikov@testpro.io>
2023-12-12 17:30:10 -0600

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -158,7 +158,7 @@ public class BaseTest {
         actions.contextClick(playlist).perform();
         WebElement editBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//nav[@class='menu playlist-item-menu']//li[contains(text(), 'Edit')]")));
         editBtn.click();
-        WebElement enterPlaylistNameField = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//li[@class='playlist playlist editing']//li")));
+        WebElement enterPlaylistNameField = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//li[@class='playlist playlist editing']//input")));
         enterPlaylistNameField.clear();
         enterPlaylistNameField.sendKeys(newPlaylistName);
         enterPlaylistNameField.sendKeys(Keys.ENTER);
