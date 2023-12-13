ba2eee81710ea4884aab2f0b90cea71ecac186b3
Andrey Zlotnikov <andrey.zlotnikov@testpro.io>
2023-12-12 21:17:13 -0600

    Update ProfileTests.java

--- src/test/java/ProfileTests.java
+++ src/test/java/ProfileTests.java
@@ -3,8 +3,9 @@ import org.testng.annotations.Test;
 public class ProfileTests extends BaseTest {
 
     @Test
-    public void renameUserName {
+    public void renameUserName () {
     logIn("andrey.zlotnikov@testpro.io", "Man07hat01tan26!");
-
+    goToProfilePage();
+    
     }
 }
