7461c1ac505eaecbe56a38a495a2290e90a78bf0
Andrey Zlotnikov <andrey.zlotnikov@testpro.io>
2023-12-13 21:20:24 -0600

    Changes

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -178,4 +178,49 @@ public class BaseTest {
         Assert.assertTrue(pageTitle.isDisplayed());
     }
 
+    public void fillInCurrentPasswordField (String currentPassword) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement currentPasswordField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id='inputProfileCurrentPassword']")));
+        currentPasswordField.click();
+        currentPasswordField.clear();
+        currentPasswordField.sendKeys(currentPassword);
+    }
+
+    public void fillInNameField (String newUserName) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement nameField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id='inputProfileName']")));
+        nameField.click();
+        nameField.clear();
+        nameField.sendKeys(newUserName);
+    }
+
+    public void fillInEmailAddress (String email) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement emailAddressField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id='inputProfileEmail']")));
+        emailAddressField.click();
+        emailAddressField.clear();
+        emailAddressField.sendKeys(email);
+    }
+
+    public void fillInNewPasswordField (String newPassword) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement newPasswordField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id='inputProfileNewPassword']")));
+        newPasswordField.click();
+        newPasswordField.clear();
+        newPasswordField.sendKeys(newPassword);
+    }
+
+    public void clickSaveBtn () {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement saveBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[class='btn-submit']")));
+        saveBtn.click();
+    }
+
+    public void assertUserNameRenamed () {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement successBunner = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'Profile updated')]")));
+        Assert.assertTrue(successBunner.isDisplayed());
+        //WebElement avatarName = wait
+
+    }
 }
